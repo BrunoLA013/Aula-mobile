a6140e716aa2155e9879f20aafd1e3b2d97e535f
Bruno Lucas <web05@agencias3.com.br>
2022-03-14 20:52:02 -0300

    Aulas 01 e 02 de mobile

---
+++ main.js
@@ -0,0 +1,33 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function () {
+        nav.classList.toggle('show')
+    })
+}
+
+/*click em item do menu = fechar*/
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('show')
+    })
+}
+
+/*muda p header quando der o scroll*/
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})
+
